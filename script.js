9abe828eb521dea2f2f4e524a9c1e8f598e7eb44
Boris <boris1488ss@gmail.com>
2020-10-22 19:49:47 +0300

    game 1.2.1

--- script.js
+++ script.js
@@ -2,15 +2,21 @@ $( document ).ready(function() {
 
   var timeout;
   var mouseStoped = false;
+  var hideHelper = false;
   $(document).mousemove(function(e){
-    if(mouseStoped === true){
+    if(mouseStoped === true && hideHelper === false){
       $(".helper").css({left:e.pageX, top:e.pageY});
     }
     clearTimeout(timeout);
     timeout = setTimeout(function(){
       mouseStoped = true;
+      hideHelper = false;
       $(".helper").css({left:e.pageX, top:e.pageY});
-    }, 3000);
+    }, 1000);
   });
+  $(document).click(function () {
+    hideHelper = true;
+    $(".helper").css({left:'-999px', top:'-999px'});
+  })
 
 });
