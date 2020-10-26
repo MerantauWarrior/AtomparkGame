9a09a53aae15677bf4321d4ba50b2f0d98eb49c7
Boris <boris1488ss@gmail.com>
2020-10-26 19:55:42 +0300

    game 3.1

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 $( document ).ready(function() {
 
+  // GAME
+  var tries = 3;
+  var endGame = false;
+  var triesText = '';
+
+  // HEELPER
   var timeout;
   var mouseStoped = false;
   var hideHelper = false;
   $(document).mousemove(function(e){
-    if(mouseStoped === true && hideHelper === false){
+    if(mouseStoped === true && hideHelper === false && endGame === false){
       $(".helper").css({left:e.pageX+46, top:e.pageY});
     }
     clearTimeout(timeout);
     timeout = setTimeout(function(){
       mouseStoped = true;
       hideHelper = false;
-      $(".helper").css({left:e.pageX+46, top:e.pageY});
+      if(endGame === false){
+        $(".helper").css({left:e.pageX+46, top:e.pageY});
+      }
     }, 3000);
   });
-  $(document).click(function () {
+  $('.tower-window').click(function () {
     hideHelper = true;
     $(".helper").css({left:'-999px', top:'-999px'});
   })
@@ -25,6 +33,11 @@ $( document ).ready(function() {
   });
   // OPEN MODALS
   $('.tower-window--2, .tower-window--4, .tower-window--6').click(function () {
+    if (endGame === true){
+      alert('You already have a discount:)')
+      return false;
+    }
+    endGame = true;
     $('.modal').removeClass('modal_opened');
     $(this).find('.tower-window__door').addClass('tower-window__door_opened');
     $('.modal-discount#'+$(this).data('id')).addClass('modal_opened');
@@ -37,9 +50,39 @@ $( document ).ready(function() {
     $('.bottomline-item#'+$(this).data('id')).addClass('bottomline-item_current');
   });
   $('.tower-window--1, .tower-window--3, .tower-window--5, .tower-window--7').click(function () {
+    if (endGame === true){
+      alert('You already have a discount:)')
+      return false;
+    }
+    if($(this).hasClass('tower-window--7')){
+      tries+1;
+    }else {
+      tries--;
+    }
+    if(tries === 2){
+      triesText = '<div class="modal__title">Уупс! К сожалению, не то окно:(</div>\n' +
+        '        <div class="modal__text">у Вас осталось 2 попытки!</div>';
+    }
+    if(tries === 1){
+      triesText = '<div class="modal__title">Увы и Ах но снова мимо...</div>\n' +
+        '        <div class="modal__text">у Вас осталась 1 попытка!</div>';
+    }
+    if(tries === 0){
+      triesText = '<div class="modal__title">Хмм...может сегодня не ваш день?</div>\n' +
+        '        <div class="modal__text">Мы дадим последний шанс</div>';
+    }
+    if($(this).hasClass('tower-window--7')){
+      triesText = '<div class="modal__title">Миау конечно...Ну да ладно!</div>\n' +
+        '        <div class="modal__text">1 бонусная попытка - саааамая последняя</div>';
+    }
+    if(tries < 0){
+      alert('No attemps left :(')
+      return false;
+    }
     $('.modal').removeClass('modal_opened');
     $(this).find('.tower-window__door').addClass('tower-window__door_opened');
     $(this).find('.monster').addClass('monster_visible');
+    $(this).children('.modal-lose').prepend(triesText);
     $(this).children('.modal-lose').delay(1000).queue(function(next){
       $(this).addClass('modal_opened');
       next();
